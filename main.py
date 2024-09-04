6d66d9f13a2118bc3cf2d246e117cc0f13e12e4f
Antoni Kedracki <antoni.kedracki@snowflake.com>
2024-09-04 13:23:10 +0200

    Twilio TURN server configured

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import os
+
 from streamlit_webrtc import webrtc_streamer
+from twilio.rest import Client
+
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+client = Client(account_sid, auth_token)
+
+token = client.tokens.create()
 
 webrtc_streamer(
     key="sample",
-    rtc_configuration={  # Add this config
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    rtc_configuration={
+        "iceServers": token.ice_servers
     }
 )
